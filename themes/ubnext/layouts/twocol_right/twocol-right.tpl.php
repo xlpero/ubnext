520ca373c3c71011c90861df4630e6100c08652d
Johan Larsson <johan.larsson@ub.gu.se>
2014-11-28 11:21:03 +0100

    changed layout to always print sidebar even if empty. Otherwise layout will break

--- themes/ubnext/layouts/twocol_right/twocol-right.tpl.php
+++ themes/ubnext/layouts/twocol_right/twocol-right.tpl.php
@@ -1,13 +1,11 @@
 
 <div class="two-col-right">
   <div class="two-col-right-inner-wrapper">
-    <?php if (!empty($content['sidebar'])): ?>
       <section id="sidebar">
         <div class="wrap">
           <?php print render($content['sidebar']); ?>
         </div>
       </section>
-    <?php endif; ?>
     <?php if (!empty($content['main'])): ?>
       <section id="main">
         <div class="wrap">
