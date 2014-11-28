4b16335e659f51bfc7f1b5e604922a897f943910
Johan Larsson <johan.larsson@ub.gu.se>
2014-11-28 15:47:07 +0100

    header added to app layouts

--- themes/ubnext/layouts/onecol_app/onecol-app.tpl.php
+++ themes/ubnext/layouts/onecol_app/onecol-app.tpl.php
@@ -1,4 +1,7 @@
 <div class="one-col-app">
+	<div class="header">
+		<?php print render($content['header']); ?>
+	</div>
 	<section class="main">
 		<?php print render($content['main']); ?>
 	</section>
