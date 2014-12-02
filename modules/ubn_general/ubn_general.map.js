4778e0c754a5ba1aa17a3ccec584cc95aab22c57
Johan Larsson <johan.larsson@ub.gu.se>
2014-12-02 13:01:01 +0100

    javascript work for maps

--- modules/ubn_general/ubn_general.map.js
+++ modules/ubn_general/ubn_general.map.js
@@ -2,8 +2,22 @@
 
     Drupal.behaviors.ubMap = {
         attach: function(context, settings) {
-            console.log(settings.ubMap);
-        }
-    }
+			console.log(settings.ubMap);
 
+			var myLatlng = new google.maps.LatLng(57.697925,11.961829);
+			var mapOptions = {
+			  center: myLatlng,
+			  zoom: 15
+			};
+			var map = new google.maps.Map(document.getElementById('ub-map-canvas'),
+			mapOptions);
+
+			var marker = new google.maps.Marker({
+			  position: myLatlng,
+			  map: map,
+			  title: 'Samhällsvetenskapliga'
+			});
+
+    	}
+    }
 })(jQuery);
