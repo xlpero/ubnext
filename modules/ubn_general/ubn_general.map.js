69a051958ece69bd885f9341760c00e2d5301e12
Johan Larsson <johan.larsson@ub.gu.se>
2014-12-02 13:50:09 +0100

    javascript fix

--- modules/ubn_general/ubn_general.map.js
+++ modules/ubn_general/ubn_general.map.js
@@ -4,20 +4,43 @@
         attach: function(context, settings) {
 			console.log(settings.ubMap);
 
-			var myLatlng = new google.maps.LatLng(57.697925,11.961829);
-			var mapOptions = {
-			  center: myLatlng,
-			  zoom: 15
-			};
-			var map = new google.maps.Map(document.getElementById('ub-map-canvas'),
-			mapOptions);
+			if (settings.ubMap.length) {
+				// set center on first item in array
+				var centerLatlng = new google.maps.LatLng(settings.ubMap[0].latitude * 1, settings.ubMap[0].longitude * 1);
+				
+				var mapOptions = {
+				  center: centerLatlng,
+				  zoom: 15
+				};
+				var map = new google.maps.Map(document.getElementById('ub-map-canvas'), mapOptions);
 
-			var marker = new google.maps.Marker({
-			  position: myLatlng,
-			  map: map,
-			  title: 'Samhällsvetenskapliga'
-			});
+				var markers = [];
+				settings.ubMap.forEach(function (item) {
+					console.log(item);
+					var latlng = new google.maps.LatLng(item.latitude, item.longitude);
+					markers.push(new google.maps.Marker({
+						position: latlng,
+						map: map,
+						title: item.label
+					}));
+				});
+				
+				if (settings.ubMap.length > 1) {
+					var new_boundary = new google.maps.LatLngBounds();
 
+					for(index in markers){
+					  position = markers[index].position;
+					  new_boundary.extend(position);
+					}
+
+					map.fitBounds(new_boundary);
+				}
+
+				
+
+
+
+			}
     	}
     }
 })(jQuery);
