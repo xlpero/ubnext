0376b6058afe6e384fb49d0323c25c81ab62c915
johan larsson <johan.larsson@ub.gu.se>
2015-09-09 13:46:44 +0200

    removed layouts not necessary

--- web/sites/default/themes/ubnext/layouts/onecol_wide/onecol-wide.tpl.php
+++
@@ -1,8 +0,0 @@
-<div class="one-col-wide">
-	<div class="header">
-		<?php print render($content['header']); ?>
-	</div>
-	<section class="main">
-	<?php print render($content['main']); ?>
-	</section>
-</div>
