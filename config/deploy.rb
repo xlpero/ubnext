c964546caea516a290883d76d4f61f72a72b5aa1
David Gustafsson <david.gustafsson@ub.gu.se>
2015-09-11 17:03:52 +0200

    Add some rudimentary capistrano deploy support

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+# config valid only for Capistrano 3.1
+lock '3.2.1'
+
+set :application, 'ubnext'
+set :repo_url, 'https://github.com/ub-digit/ubnext.git'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :branch, 'master'
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/srv/www/drupal7/ubnext'
+
+set :scm, :git
+set :format, :pretty
+set :log_level, :debug
+
+# Default false
+set :pty, false
+
+set :linked_files, %w{web/sites/default/secret.settings.php}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+namespace :deploy do
+end
