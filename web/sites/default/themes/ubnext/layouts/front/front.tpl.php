192379d0b83e70fe57b2dc9ee2e81de5a472f40b
johan larsson <johan.larsson@ub.gu.se>
2015-09-17 13:25:08 +0200

    played to understand widgets

--- web/sites/default/themes/ubnext/layouts/front/front.tpl.php
+++ web/sites/default/themes/ubnext/layouts/front/front.tpl.php
@@ -1,11 +1,4 @@
 <div class="container">
-	<div class="row">
-		<div class="col-xs-12">
-			This is the frontpage under development
-		</div>
-	</div>
-
-
 	<?php if (!empty($content['main'])): ?>
 		<div class="row">
 			<div class="col-xs-12">
