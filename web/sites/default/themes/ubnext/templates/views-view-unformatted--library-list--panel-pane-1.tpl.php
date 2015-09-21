3c6c8046225a3b1404d1c31a2d1976ba9b3d2015
johan larsson <johan.larsson@ub.gu.se>
2015-09-21 15:36:59 +0200

    library view and list of items updated

---
+++ web/sites/default/themes/ubnext/templates/views-view-unformatted--library-list--panel-pane-1.tpl.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * - $title : The title of this group of rows.  May be empty.
+ * - $options['type'] will either be ul or ol.
+ * @ingroup views_templates
+ */
+?>
+
+
+
+<ul class="list-unstyled ub-list-of-links">
+	<?php foreach ($rows as $id => $row): ?>
+	  <li><?php print $row; ?></li>
+	<?php endforeach; ?>
+</ul>
+
