190652c935c4cf4e164f0bda1e5d76f89d50bf56
johan larsson <johan.larsson@ub.gu.se>
2015-10-13 09:55:24 +0200

    various fixes

--- web/sites/default/themes/ubnext/templates/views-view-unformatted--library-list--libraries.tpl.php
+++ web/sites/default/themes/ubnext/templates/views-view-unformatted--library-list--libraries.tpl.php
@@ -9,12 +9,31 @@
  * @ingroup views_templates
  */
 ?>
-<div class="row">
-  <div class="col-sm-offset-8 col-sm-4 hidden-xs library-libraries-opening-hours-header"><strong><?php print t('Opening hours'); ?></strong></div>
-</div>
-<ul class="row list-unstyled ub-list-of-links">
-	<?php foreach ($rows as $id => $row): ?>
-	  <li class="col-sm-8 col-sm-offset-2"><?php print $row; ?></li>
-	<?php endforeach; ?>
-</ul>
 
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 col-sm-8 col-sm-offset-2">
+			<h2 class="center"><?php print $view->get_title(); ?></h2>
+			<div class="row">
+					<div class="col-sm-11">
+						<div class="row">
+							<div class="col-sm-offset-6 col-sm-6 hidden-xs library-libraries-opening-hours-header">
+								<div class="row">
+									<div class="col-xs-12">
+										<strong><?php print t('Opening hours'); ?></strong>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+			</div>
+			<ul class="list-unstyled ub-list-of-links"> 
+				<?php foreach ($rows as $id => $row): ?>
+				  <li><?php print $row; ?></li>
+				<?php endforeach; ?>
+			</ul>
+		</div>
+	</div>
+</div>
