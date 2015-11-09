40508af3c59ce9592796c4e1cd86b09f6d9873e6
David Gustafsson <david.gustafsson@ub.gu.se>
2015-11-09 16:54:40 +0100

    Add example drupal settings files

---
+++ settings/secret.settings.php
@@ -0,0 +1,20 @@
+<?php
+$databases = array (
+  'default' =>
+   array (
+     'default' =>
+      array (
+       'database' => 'drupal7_ubnext',
+       'username' => 'drupal7_ubnext',
+       'password' => 'drupal7_ubnext',
+       'host' => 'localhost',
+       'port' => '',
+       'driver' => 'mysql',
+       'prefix' => '',
+     ),
+   ),
+);
+
+$drupal_hash_salt = 'set me to something random';
+$conf['ubn_summon_access_id'] = '';
+$conf['ubn_summon_secret_key'] = '';
---
+++ settings/site.settings.php
@@ -0,0 +1,7 @@
+<?php
+// UBN environment. For local development environments, set to something unique
+// for example the the unix account on intra
+$conf['ubn_environment'] = 'xaccount';
+$conf['file_temporary_path'] = '/home/drupal-deploy/tmp/drupal7_ubn_' . $conf['ubn_environment'];
+
+$conf['ubn_settings_solr_host'] = '130.241.35.208';
