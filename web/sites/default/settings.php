8e866d709568982ed8a09d197a0f2341b705a661
johan larsson <johan.larsson@ub.gu.se>
2015-11-11 11:04:16 +0100

    updatade general to use variable translations and removed header from library properties when zero

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,3 +1,20 @@
 <?php
 include dirname(__FILE__) . DIRECTORY_SEPARATOR . 'secret.settings.php';
 include dirname(__FILE__) . DIRECTORY_SEPARATOR . 'site.settings.php';
+
+/**
+ * Multilingual settings
+ *
+ * This is a collection of variables that can be set up for each language when
+ * internationalization (i18n) is enabled. These are the basic ones for Drupal
+ * core, but you can add your own here.
+ */
+$conf['i18n_variables'] = array(
+  'site_name',
+  'site_slogan',
+  'site_mission',
+  'site_footer',
+  'anonymous',
+  'menu_primary_menu',
+  'menu_secondary_menu',
+);
