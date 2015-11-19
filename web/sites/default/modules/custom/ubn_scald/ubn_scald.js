861a1a0c0e0abbfe7c638bd896d17df35f9fa6c4
johan larsson <johan.larsson@ub.gu.se>
2015-11-19 16:06:22 +0100

    video fix and summontesting

--- web/sites/default/modules/custom/ubn_scald/ubn_scald.js
+++ web/sites/default/modules/custom/ubn_scald/ubn_scald.js
@@ -1,13 +1,10 @@
 (function ($) {
   Drupal.behaviors.ubn_scald = {
     attach: function(context, settings) {
-      //TODO: make sure can be clicked only once
       $('.ubn-video-play-button', context).click(function(e) {
         $video = $(this).parents('.ubn-video');
-        //TODO: attach behaviors thing (check scald)
         var wrapped_player = $('<div></div>').html($video.data('player-html'));
         Drupal.attachBehaviors(wrapped_player, settings);
-        //$self.html(wrapped_player.contents());
         $video.html(wrapped_player.contents());
       });
     }
