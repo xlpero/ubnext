680705754caba17b277265d9e348e50210725139
johan larsson <johan.larsson@ub.gu.se>
2016-01-13 10:47:20 +0100

    new sidebar-left layout

---
+++ web/sites/default/themes/ubnext/layouts/onecol_sidebar_left/onecol-sidebar-left.tpl.php
@@ -0,0 +1,9 @@
+<div class="container">
+  <aside class="sidebar col-sm-4">
+    <?php print render($content['sidebar']); ?>
+  </aside>
+  <section class="main col-sm-8">
+    Main
+  	<?php print render($content['main']); ?>
+  </section>
+</div>
