ba17d8b4c1b29faa70fca6df40f28e1d79f5aabe
David Gustafsson <david.gustafsson@ub.gu.se>
2016-01-27 15:34:18 +0100

    Implement proper solr suggester support

--- web/sites/default/modules/custom/ubn_search_api_solr/src/UBNSearchApiAutocompleteServerSuggester.php
+++ web/sites/default/modules/custom/ubn_search_api_solr/src/UBNSearchApiAutocompleteServerSuggester.php
@@ -16,21 +16,44 @@ class UBNSearchApiAutocompleteServerSuggester extends SearchApiAutocompleteServe
   /**
    * {@inheritdoc}
    */
-  /*
   public function defaultConfiguration() {
-    return array(
-      'fields' => array(
-        //'autocomplete' => 1,?
+    $index = $this->getSearch()->index();
+    return parent::defaultConfiguration() + array(
+      'ubn_solr_server' => array(
+        'suggester_path' => 'autocomplete',
+        'suggester_dictionaries' => $index->machine_name . '_infix',
       ),
     );
   }
-  */
 
   /**
    * {@inheritdoc}
    */
   public function buildConfigurationForm(array $form, array &$form_state) {
     $form = parent::buildConfigurationForm($form, $form_state);
+
+    //TODO: namespacing?
+    $form['ubn_solr_server'] = array(
+      '#type' => 'fieldset',
+      '#title' => t('UBN Solr Server'),
+      '#desciption' => t('UBN Solr Server'),
+      '#collapsible' => TRUE,
+      '#collapsed' => FALSE,
+      '#tree' => TRUE,
+    );
+    $form['ubn_solr_server']['suggester_path'] = array(
+      '#title' => t('Suggester request handler path'),
+      '#type' => 'textfield',
+      '#description' => 'todo',
+      '#default_value' => $this->configuration['ubn_solr_server']['suggester_path'],
+    );
+    //TODO: support for multiple dicts
+    $form['ubn_solr_server']['suggester_dictionaries'] = array(
+      '#title' => t('Suggester dictionaries'),
+      '#type' => 'textarea',
+      '#description' => 'One dictionary per line, in order of preference.',
+      '#default_value' => $this->configuration['ubn_solr_server']['suggester_dictionaries'],
+    );
     return $form;
   }
 
@@ -38,6 +61,12 @@ class UBNSearchApiAutocompleteServerSuggester extends SearchApiAutocompleteServe
    * {@inheritdoc}
    */
   public function getAutocompleteSuggestions(SearchApiQueryInterface $query, $incomplete_key, $user_input) {
-    return $query->getIndex()->server()->getAutocompleteSuggestions($query, $this->getSearch(), $incomplete_key, $user_input);
+    return $query->getIndex()->server()->getAutocompleteSuggestions(
+      $query,
+      $this->getSearch(),
+      $incomplete_key,
+      $user_input,
+      $this->configuration['ubn_solr_server']
+    );
   }
 }
