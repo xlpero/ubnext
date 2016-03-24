e62d1310dcec4cc15081c50c32d7185da73b0089
David Gustafsson <david.gustafsson@ub.gu.se>
2016-03-24 12:26:19 +0100

    Remove debug output

--- web/sites/default/themes/ubnext/js/main.js
+++ web/sites/default/themes/ubnext/js/main.js
@@ -1,8 +1,6 @@
 (function ($) {
   Drupal.behaviors.ubnext_fitvids = {
     attach: function(context, settings) {
-      console.log('attaching content');
-      console.dir($('.ubn-video-player', context));
       $('.ubn-video-player', context).fitVids();
     }
   }
