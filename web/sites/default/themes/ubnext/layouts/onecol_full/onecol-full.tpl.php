5174ab914dd7f79e488e16eb38a52600a15fe8d0
David Gustafsson <david.gustafsson@ub.gu.se>
2016-04-18 10:46:00 +0200

    Fix header container in onecol layout

--- web/sites/default/themes/ubnext/layouts/onecol_full/onecol-full.tpl.php
+++ web/sites/default/themes/ubnext/layouts/onecol_full/onecol-full.tpl.php
@@ -1,14 +1,14 @@
 <div class="main">
   <div class="container">
-    <div class="row ub-panel-separator">
+  <?php if (!empty ($content['header'])) : ?>
+    <div class="row">
+      <div class="col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2">
+        <?php print render($content['header']); ?>
+      </div>
+    </div>
+  <?php endif; ?>
 
-    <?php if (!empty ($content['header'])) : ?>
-      <?php print render($content['header']); ?>
-    <?php endif; ?>
-
-    <?php if (!empty ($content['main'])) : ?>
-      <?php print render($content['main']); ?>
-    <?php endif; ?>
-
-  </div>
+  <?php if (!empty ($content['main'])) : ?>
+    <?php print render($content['main']); ?>
+  <?php endif; ?>
 </div>
