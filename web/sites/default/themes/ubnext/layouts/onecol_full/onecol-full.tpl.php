8f672269b39d8729095df27f198caad3a3749819
David Gustafsson <david.gustafsson@ub.gu.se>
2016-04-18 15:15:33 +0200

    Refactor one column layout with multiple containers

--- web/sites/default/themes/ubnext/layouts/onecol_full/onecol-full.tpl.php
+++ web/sites/default/themes/ubnext/layouts/onecol_full/onecol-full.tpl.php
@@ -1,14 +1,36 @@
 <div class="main">
   <div class="container">
-  <?php if (!empty ($content['header'])) : ?>
-    <div class="row">
-      <div class="col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2">
-        <?php print render($content['header']); ?>
+    <?php if (!empty ($content['header'])) : ?>
+      <div class="row">
+        <div class="col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2">
+          <?php print render($content['header']); ?>
+        </div>
       </div>
-    </div>
-  <?php endif; ?>
+    <?php endif; ?>
+  </div>
 
-  <?php if (!empty ($content['main'])) : ?>
-    <?php print render($content['main']); ?>
-  <?php endif; ?>
+  <div class="container fullwidth-on-xs">
+    <?php if (!empty ($content['promoted_top'])) : ?>
+      <?php print render($content['promoted_top']); ?>
+    <?php endif; ?>
+  </div>
+
+  <div class="container">
+    <?php if (!empty ($content['main_top'])) : ?>
+      <?php print render($content['main_top']); ?>
+    <?php endif; ?>
+  </div>
+
+  <div class="container fullwidth-on-xs">
+    <?php if (!empty ($content['promoted'])) : ?>
+      <?php print render($content['promoted']); ?>
+    <?php endif; ?>
+  </div>
+
+  <div class="container">
+    <?php if (!empty ($content['main'])) : ?>
+      <?php print render($content['main']); ?>
+    <?php endif; ?>
+  </div>
+
 </div>
