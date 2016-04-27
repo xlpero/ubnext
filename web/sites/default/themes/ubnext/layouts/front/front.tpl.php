4a59598156ad7018c0db33021b54b10ab5de2d82
johan larsson <johan.larsson@ub.gu.se>
2016-04-27 13:37:19 +0200

    html fixes for section

--- web/sites/default/themes/ubnext/layouts/front/front.tpl.php
+++ web/sites/default/themes/ubnext/layouts/front/front.tpl.php
@@ -1,16 +1,22 @@
 
 <?php if (!empty($content['services'])): ?>
-
+	<section id="services" class="container">
+		<div class="row">
+			<div class="col-xs-12 col-md-10 col-md-offset-1">
+				<?php print render($content['services']); ?>
+			</div>
+		</div>
+	</section>
 <?php endif ?>
 
 
 <?php if (!empty($content['main'])): ?>
-	<div class="container">
+	<section id="main" class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-8 col-sm-offset-2">
 		  		<?php print render($content['main']); ?>
 		  	</div>
 		</div>
-	</div>
+	</section>
 <?php endif ?>
 
