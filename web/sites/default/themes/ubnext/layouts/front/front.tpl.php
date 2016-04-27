d1483035bbb14714fbc267968a9af8bb3050e946
johan larsson <johan.larsson@ub.gu.se>
2016-04-27 13:37:04 +0200

    new region

--- web/sites/default/themes/ubnext/layouts/front/front.tpl.php
+++ web/sites/default/themes/ubnext/layouts/front/front.tpl.php
@@ -1,9 +1,16 @@
-<div class="container">
-	<?php if (!empty($content['main'])): ?>
+
+<?php if (!empty($content['services'])): ?>
+
+<?php endif ?>
+
+
+<?php if (!empty($content['main'])): ?>
+	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-8 col-sm-offset-2">
 		  		<?php print render($content['main']); ?>
 		  	</div>
 		</div>
-	<?php endif ?>
-</div>
+	</div>
+<?php endif ?>
+
