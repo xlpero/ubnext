4e72950de580eaac15cec198f5d52eba8dd76c76
johan larsson <johan.larsson@ub.gu.se>
2016-05-16 13:39:59 +0200

    added code for new field and styles for the new 'kort om' on basic page

--- web/sites/default/modules/features/ubn_terms/js/ubn_terms.js
+++ web/sites/default/modules/features/ubn_terms/js/ubn_terms.js
@@ -32,11 +32,11 @@
       $collapsibles = $('.term-collapsible', context);
       $collapsibles.on('show.bs.collapse', function() {
         //Showing
-        $(this).find('.fa').removeClass('fa-chevron-down').addClass('fa-chevron-up');
+        $(this).find('.fa.fa-dynamic').removeClass('fa-chevron-down').addClass('fa-chevron-up');
       });
       $collapsibles.on('hide.bs.collapse', function() {
         //Hiding
-        $(this).find('.fa').removeClass('fa-chevron-up').addClass('fa-chevron-down');
+        $(this).find('.fa.fa-dynamic').removeClass('fa-chevron-up').addClass('fa-chevron-down');
       });
 
 
