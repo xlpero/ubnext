70e663a1dfd29d227624046bb230e81e02828616
David Gustafsson <david.gustafsson@ub.gu.se>
2016-06-13 14:55:19 +0200

    Fix indention

--- web/sites/default/themes/ubnext/layouts/front/front.tpl.php
+++ web/sites/default/themes/ubnext/layouts/front/front.tpl.php
@@ -1,41 +1,41 @@
 <?php if (!empty($content['search'])): ?>
-	<div id="search" class="container panel-separator">
-		<div class="row">
-			<div class="col-xs-12 col-sm-8 col-sm-offset-2">
-		  		<?php print render($content['search']); ?>
-		  	</div>
-		</div>
-	</div>
+  <div id="search" class="container panel-separator">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+        <?php print render($content['search']); ?>
+      </div>
+    </div>
+  </div>
 <?php endif ?>
 
 <?php if (!empty($content['services'])): ?>
-	<div id="services" class="container  panel-separator">
-		<div class="row">
-			<div class="col-xs-12 col-md-10 col-md-offset-1">
-				<?php print render($content['services']); ?>
-			</div>
-		</div>
-	</div>
+  <div id="services" class="container  panel-separator">
+    <div class="row">
+      <div class="col-xs-12 col-md-10 col-md-offset-1">
+        <?php print render($content['services']); ?>
+      </div>
+    </div>
+  </div>
 <?php endif ?>
 
 <?php if (!empty($content['news'])): ?>
-	<div id="news" class="container panel-separator">
-		<div class="row">
-			<div class="col-xs-12 col-sm-6">
-				<?php print render($content['news']); ?>
-			</div>
-		</div>
-	</div>
+  <div id="news" class="container panel-separator">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <?php print render($content['news']); ?>
+      </div>
+    </div>
+  </div>
 <?php endif ?>
 
 <?php if (!empty($content['main'])): ?>
-	<div id="main" class="container panel-separator">
-		<div class="row">
-			<div class="col-xs-12 col-sm-8 col-sm-offset-2">
-		  		<?php print render($content['main']); ?>
-		  	</div>
-		</div>
-	</div>
+  <div id="main" class="container panel-separator">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+        <?php print render($content['main']); ?>
+      </div>
+    </div>
+  </div>
 <?php endif ?>
 
 <div class="container fullwidth-on-xs">
