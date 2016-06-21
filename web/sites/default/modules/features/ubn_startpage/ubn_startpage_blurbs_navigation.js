7dc1061be8eef29c365a12e686f4fdaef7a6014c
David Gustafsson <david.gustafsson@ub.gu.se>
2016-06-21 15:11:51 +0200

    Remove min-height and add basic responsive collapsible behaviour

--- web/sites/default/modules/features/ubn_startpage/ubn_startpage_blurbs_navigation.js
+++ web/sites/default/modules/features/ubn_startpage/ubn_startpage_blurbs_navigation.js
@@ -1,9 +1,12 @@
 (function($) {
+  // Not needed since general pane behaviour takes care of this
+  /*
   Drupal.behaviors.ubn_startpage_blurbs_navigation_equal_heights = {
     attach: function(context, settings) {
       $('.blurb-navigation', context).matchHeight();
     }
   };
+  */
   Drupal.behaviors.ubn_startpage_blurbs_navigation_show_more = {
     attach: function(context, settings) {
       var max_items = 5;
@@ -11,35 +14,38 @@
       var hide_text = Drupal.t('Hide');
       $('.blurb-navigation', context).each(function() {
         $this = $(this);
-        $blurb_container = $this.closest('.blurb-navigation');
+        var $blurb_container = $this.closest('.blurb-navigation');
 
         var $extra_items = $('.ubn-panel-links-link-list li:gt(' + (max_items - 1) + ')', $this);
 
         if ($extra_items.length) {
-          $extra_items.hide();
+          // Set initial state
+          //$extra_items.hide();
+          $blurb_container.addClass('collapsed');
+
           //TODO: drupal js-theme function?
           var $show_more_link = $('<div class="blurb-navigation-show-more"><a href="javascript:void(0);">' + show_all_text + '</a> <i class="fa fa-chevron-down aria-hidden="true"></div>');
 
-          // Set initial state
           $('.ubn-panel-links-link-list', $this).after($show_more_link);
 
           var $show_more_link_a = $show_more_link.find('a');
           var $show_more_link_i = $show_more_link.find('i');
           $show_more_link.click(function() {
             //TODO: namespaced expanded-class?
-            if ($blurb_container.hasClass('expanded')) {
-              $extra_items.hide();
+            if (!$blurb_container.hasClass('collapsed')) {
+              //$extra_items.hide();
               $show_more_link_a.text(show_all_text);
               $show_more_link_i.removeClass('fa-chevron-up');
               $show_more_link_i.addClass('fa-chevron-down');
-              $blurb_container.removeClass('expanded');
+              $blurb_container.addClass('collapsed');
             }
             else {
-              $extra_items.fadeIn(200); //or just .hide()?
+              //$extra_items.fadeIn(200); //or just .hide()?
+              //$extra_items.show();
               $show_more_link_a.text(hide_text);
               $show_more_link_i.removeClass('fa-chevron-down');
               $show_more_link_i.addClass('fa-chevron-up');
-              $blurb_container.addClass('expanded');
+              $blurb_container.removeClass('collapsed');
             }
             $.fn.matchHeight._update(false);
           });
