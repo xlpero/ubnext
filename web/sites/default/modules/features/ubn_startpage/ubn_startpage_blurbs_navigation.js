b215e21cc5459b47071bce62c2c9d9de3642fe01
David Gustafsson <david.gustafsson@ub.gu.se>
2016-06-22 11:45:59 +0200

    Add responsive count for start page navigation blurb items

--- web/sites/default/modules/features/ubn_startpage/ubn_startpage_blurbs_navigation.js
+++ web/sites/default/modules/features/ubn_startpage/ubn_startpage_blurbs_navigation.js
@@ -9,32 +9,46 @@
   */
   Drupal.behaviors.ubn_startpage_blurbs_navigation_show_more = {
     attach: function(context, settings) {
-      var max_items = 5;
+      var sm_threshold = 3;
+      var md_threshold = 5;
       var show_all_text = Drupal.t('Show more');
       var hide_text = Drupal.t('Hide');
+
       $('.blurb-navigation', context).each(function() {
         $this = $(this);
         var $blurb_container = $this.closest('.blurb-navigation');
 
-        var $extra_items = $('.ubn-panel-links-link-list li:gt(' + (max_items - 1) + ')', $this);
+        var $extra_items = $('.ubn-panel-links-link-list li:gt(' + (sm_threshold - 1) + ')', $this);
 
         if ($extra_items.length) {
           // Set initial state
           //$extra_items.hide();
           $blurb_container.addClass('collapsed');
+          $blurb_container.addClass('collapsible');
 
           //TODO: drupal js-theme function?
-          var $show_more_link = $('<div class="blurb-navigation-show-more"><a href="javascript:void(0);">' + show_all_text + '</a> <i class="fa fa-chevron-down aria-hidden="true"></div>');
+          var $show_more_link = $([
+              '<div class="blurb-navigation-show-more">',
+              '<a href="javascript:void(0);">',
+              '<span class="blurb-navigation-show-more-label">',
+              show_all_text,
+              '</span>',
+              ' (<span class="blurb-navigation-show-more-count"></span>)',
+              '</a>',
+              ' <i class="fa fa-chevron-down aria-hidden="true">',
+              '</div>'
+            ].join('')
+          );
 
           $('.ubn-panel-links-link-list', $this).after($show_more_link);
 
-          var $show_more_link_a = $show_more_link.find('a');
+          var $show_more_link_a_label = $show_more_link.find('.blurb-navigation-show-more-label');
           var $show_more_link_i = $show_more_link.find('i');
           $show_more_link.click(function() {
             //TODO: namespaced expanded-class?
             if (!$blurb_container.hasClass('collapsed')) {
               //$extra_items.hide();
-              $show_more_link_a.text(show_all_text);
+              $show_more_link_a_label.text(show_all_text);
               $show_more_link_i.removeClass('fa-chevron-up');
               $show_more_link_i.addClass('fa-chevron-down');
               $blurb_container.addClass('collapsed');
@@ -42,7 +56,7 @@
             else {
               //$extra_items.fadeIn(200); //or just .hide()?
               //$extra_items.show();
-              $show_more_link_a.text(hide_text);
+              $show_more_link_a_label.text(hide_text);
               $show_more_link_i.removeClass('fa-chevron-down');
               $show_more_link_i.addClass('fa-chevron-up');
               $blurb_container.removeClass('collapsed');
@@ -51,6 +65,29 @@
           });
         }
       });
+
+      var sm_mql = window.matchMedia('(max-width: 768px)');
+      var set_items_count = function(threshold) {
+        $('.blurb-navigation.collapsible', context).each(function() {
+          var $this = $(this);
+          var items_count = $('li', $this).length;
+          if(items_count > threshold) {
+            var hidden_items = items_count - threshold;
+            $('.blurb-navigation-show-more-count', $this).text(hidden_items);
+          }
+        });
+      }
+
+      var sm_mql_handler = function(mql) {
+        if(mql.matches) {
+          set_items_count(sm_threshold);
+        }
+        else {
+          set_items_count(md_threshold);
+        }
+      };
+      sm_mql.addListener(sm_mql_handler);
+      sm_mql_handler(sm_mql);
     }
   }
 })(jQuery);
