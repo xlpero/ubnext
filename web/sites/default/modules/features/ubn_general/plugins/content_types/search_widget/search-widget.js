fb2d77b9ccb5c590b8b8f40e02057a748ad0ef17
larsberglen <lars.berglen@ub.gu.se>
2018-09-20 10:40:44 +0200

    emty search now gives starting page in Primo

--- web/sites/default/modules/features/ubn_general/plugins/content_types/search_widget/search-widget.js
+++ web/sites/default/modules/features/ubn_general/plugins/content_types/search_widget/search-widget.js
@@ -11,8 +11,9 @@
       var lang = $('html').attr('lang');
       lang = lang == 'sv' ? 'sv_SE' : 'en_US';
       var query = $('.custom-form input[name="query"]').val();
+      var seachCriteria = query.length > 0 ? 'any,contains,' : '';
       query = encodeURIComponent(query);
-      var url = 'https://gu-se-primo.hosted.exlibrisgroup.com/primo-explore/search?query=any,contains,' + query + '&vid=46GUB_VU1&search_scope=default_scope&sortby=rank&lang=' + lang;
+      var url = 'https://gu-se-primo.hosted.exlibrisgroup.com/primo-explore/search?query=' + seachCriteria + query + '&vid=46GUB_VU1&search_scope=default_scope&sortby=rank&lang=' + lang;
       window.location.href = url;
     });
   });
