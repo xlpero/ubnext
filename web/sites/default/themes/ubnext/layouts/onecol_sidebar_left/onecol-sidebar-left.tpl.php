cd7739d92690d51ab33a861a92c8d1cf29ab2bd2
Johan Larsson <johan.larsson@ub.gu.se>
2018-11-30 14:01:28 +0100

    new test to see if this works for rplay

--- web/sites/default/themes/ubnext/layouts/onecol_sidebar_left/onecol-sidebar-left.tpl.php
+++ web/sites/default/themes/ubnext/layouts/onecol_sidebar_left/onecol-sidebar-left.tpl.php
@@ -17,7 +17,7 @@
     </div>
   <?php endif; ?>
 
-  <div class="row rs_preserve">
+  <div class="row rs_preserve rs_skip">
     <?php if (!empty ($content['search_bar_top'])) : ?>
       <div class="searchbar-top col-xs-12 col-sm-10 col-md-8 col-md-offset-2 col-sm-offset-1">
         <?php print render($content['search_bar_top']); ?>
@@ -25,21 +25,21 @@
     <?php endif; ?>
   </div>
   </div>
-  <div class="row ajax-container">
+  <div class="row ajax-containerrs_preserve  rs_skip">
     <?php if (!empty ($content['sidebar'])) : ?>
     <aside class="sidebar  col-xs-12 col-sm-4">
-      <div class="facet-filter">
+      <div class="facet-filter rs_preserve">
         <?php print render($content['sidebar']); ?>
       </div>
     </aside>
     <?php endif; ?>
 
     <?php if (!empty ($content['sidebar'])) : ?>
-      <div class="main col-xs-12 col-sm-8">
+      <div class="main col-xs-12 col-sm-8 rs_preserve rs_skip">
     <?php else : ?>
-      <div class="main col-xs-12 col-sm-8 col-sm-offset-2 center no-sidebar">
+      <div class="main col-xs-12 col-sm-8 col-sm-offset-2 center no-sidebar rs_preserve rs_skip">
     <?php endif; ?>
-      <div class="main-inner">
+      <div class="main-inner rs_preserve rs_skip">
     	   <?php print render($content['main']); ?>
       </div>
     </div>
