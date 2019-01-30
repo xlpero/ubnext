279bac8f391b8709b08ed0681e6742cfd59e6104
larsberglen <lars.berglen@ub.gu.se>
2019-01-30 16:37:59 +0100

    Minor fixes on news blurbs. New style for missing image, added header on start page. blurb bottom-margin in tablet view

--- web/sites/default/modules/features/ubn_startpage/ubn_startpage.js
+++ web/sites/default/modules/features/ubn_startpage/ubn_startpage.js
@@ -5,22 +5,7 @@
         $('.blurb', context).matchHeight();
       }
 
-       // mouse enter on promoted story
-      $('div.story-promoted-container').on('mouseenter',function(){
-      	$(this).css('cursor', 'pointer');
-      	$(this).find('a').css('text-decoration', 'underline');
-  		});
-  		// mouse leave on promoted story
-  		$('div.story-promoted-container').on('mouseleave',function(){
-      	$(this).css('cursor', 'default');
-      	$(this).find('a').css('text-decoration', 'none');
-
-  		});
-
-  		$('div.story-promoted-container').on('click',function(){
-  			let url = $(this).find('a').attr("href");
-        window.location.href = url;
-  		});
+     
     }
   };
 })(jQuery);
