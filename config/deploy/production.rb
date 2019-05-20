5c8f140a749ffd22a880768d0dbfc7e141e7ba04
Per Larsson <per.larsson@ub.gu.se>
2019-05-20 12:28:36 +0200

    Set release tag for production and correct tag name in deploy.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -26,7 +26,7 @@ server 'beta.ub.gu.se', user: 'drupal-deploy', roles: %w{app},
 set :deploy_to, '/var/www/drupal/production'
 
 # Set release tag
-set :branch, 'release-2019.04-002'
+set :branch, 'release-2019.05-004'
 
 
 
